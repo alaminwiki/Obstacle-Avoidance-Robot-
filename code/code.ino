16768e076d96dc2f37914ea2f675115cb6f052f0
Md. Al Amin <alaminwiki@gmail.com>
2024-11-24 23:46:59 +0600

    Added arduino file

--- code/code.ino
+++ code/code.ino
@@ -1,4 +1,6 @@
 // Define pins for L298 motor driver
+
+int trig = 5, echo = 6;
 int motorA1 = 8;  // IN1
 int motorA2 = 9;  // IN2
 int motorB1 = 10; // IN3
@@ -10,45 +12,65 @@ void setup() {
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
   pinMode(motorB2, OUTPUT);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
 
   // Initialize serial communication with Bluetooth
   Serial.begin(9600);  // Default baud rate for HC-05 Bluetooth module
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();  // Read the Bluetooth data
-
-    // Control motors based on the Bluetooth command
-    if (command == 'F') { // Forward
-      digitalWrite(motorA1, HIGH);
-      digitalWrite(motorA2, LOW);
-      digitalWrite(motorB1, HIGH);
-      digitalWrite(motorB2, LOW);
-    }
-    else if (command == 'B') { // Backward
-      digitalWrite(motorA1, LOW);
-      digitalWrite(motorA2, HIGH);
-      digitalWrite(motorB1, LOW);
-      digitalWrite(motorB2, HIGH);
-    }
-    else if (command == 'L') { // Left
-      digitalWrite(motorA1, LOW);
-      digitalWrite(motorA2, HIGH);
-      digitalWrite(motorB1, HIGH);
-      digitalWrite(motorB2, LOW);
-    }
-    else if (command == 'R') { // Right
-      digitalWrite(motorA1, HIGH);
-      digitalWrite(motorA2, LOW);
-      digitalWrite(motorB1, LOW);
-      digitalWrite(motorB2, HIGH);
-    }
-    else if (command == 'S') { // Stop
-      digitalWrite(motorA1, LOW);
-      digitalWrite(motorA2, LOW);
-      digitalWrite(motorB1, LOW);
-      digitalWrite(motorB2, LOW);
-    }
+  int distance=calculateDistance(trig,echo);
+  //Serial.println(distance);
+  if(distance < 17){
+    right();
   }
+ else{forward();}
+  
+}
+int calculateDistance(int trig_pin,int echo_pin)
+{
+  digitalWrite(trig_pin, LOW);
+  delay(2);
+  digitalWrite(trig_pin, HIGH);
+  delay(10);
+  digitalWrite(trig_pin,LOW);
+  int duration = pulseIn(echo_pin,HIGH);
+  int distance=(0.034*duration)/2;
+  return distance;
+}
+
+void backward(){
+  digitalWrite(motorA1, HIGH);
+      digitalWrite(motorA2, LOW);
+      digitalWrite(motorB1, HIGH);
+      digitalWrite(motorB2, LOW);
+
+}
+void forward(){
+  digitalWrite(motorA1, LOW);
+      digitalWrite(motorA2, HIGH);
+      digitalWrite(motorB1, LOW);
+      digitalWrite(motorB2, HIGH);
+
+}
+void left(){
+  digitalWrite(motorA1, LOW);
+      digitalWrite(motorA2, HIGH);
+      digitalWrite(motorB1, HIGH);
+      digitalWrite(motorB2, LOW);
+}
+void right(){
+  digitalWrite(motorA1, HIGH);
+      digitalWrite(motorA2, LOW);
+      digitalWrite(motorB1, LOW);
+      digitalWrite(motorB2, HIGH);
+
+}
+void stop(){
+  digitalWrite(motorA1, LOW);
+      digitalWrite(motorA2, LOW);
+      digitalWrite(motorB1, LOW);
+      digitalWrite(motorB2, LOW);
+
 }
